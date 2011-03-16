972ab592d30a2df84f37e66613871bf1173bf260
ornicar <thibault.duplessis@gmail.com>
2011-03-15 20:05:32 -0700

    Add SignedCommentInterface

---
+++ Model/SignedCommentInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace FOS\CommentBundle\Model;
+
+use FOS\UserBundle\Model\UserInterface;
+
+/**
+ * A signed comment is bound to a FOS\UserBundle User model.
+ */
+interface SignedCommentInterface
+{
+    /**
+     * Sets the author of the Comment
+     *
+     * @return UserInterface
+     */
+    function getAuthor();
+
+    /**
+     * Sets the author of the Comment
+     *
+     * @param UserInterface $user
+     */
+    function setAuthor(UserInterface $user);
+}
