44e6efeefedbc98752fecc5a3f99368ff74627c9
ornicar <thibault.duplessis@gmail.com>
2011-03-29 22:39:22 -0700

    Synchronize with upstream

--- Controller/ThreadController.php
+++ Controller/ThreadController.php
@@ -35,7 +35,7 @@ class ThreadController extends ContainerAware
         $form = $this->container->get('fos_comment.form_factory.comment')->createForm();
         $form->setData($comment);
 
-        return $this->container->get('templating')->renderResponse('FOSCommentBundle:Thread:show.html.twig', array(
+        return $this->container->get('templating')->renderResponse('FOSComment:Thread:show.html.twig', array(
             'thread' => $thread,
             'displayDepth'  => $displayDepth,
             'form'   => $form
@@ -54,7 +54,7 @@ class ThreadController extends ContainerAware
             throw new NotFoundHttpException(sprintf('No comment thread with identifier "%s"', $identifier));
         }
 
-        return $this->container->get('templating')->renderResponse('FOSCommentBundle:Thread:showFeed.xml.twig', array(
+        return $this->container->get('templating')->renderResponse('FOSComment:Thread:showFeed.xml.twig', array(
             'thread' => $thread
         ));
     }
