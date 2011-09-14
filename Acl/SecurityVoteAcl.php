35b756dfa3e03efbb2e853ea7faeeb1b13631747
Derek Stobbe <djstobbe@gmail.com>
2011-09-14 17:29:45 -0600

    Use Vote#getVoter() instead of Vote#getAuthor()

--- Acl/SecurityVoteAcl.php
+++ Acl/SecurityVoteAcl.php
@@ -141,7 +141,7 @@ class SecurityVoteAcl implements VoteAclInterface
         $acl = $this->aclProvider->createAcl($objectIdentity);
 
         if ($vote instanceof SignedVoteInterface) {
-            $securityIdentity = UserSecurityIdentity::fromAccount($vote->getAuthor());
+            $securityIdentity = UserSecurityIdentity::fromAccount($vote->getVoter());
             $acl->insertObjectAce($securityIdentity, MaskBuilder::MASK_OWNER);
         }
 
