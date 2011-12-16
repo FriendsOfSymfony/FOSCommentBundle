dd907f51931f4b71a6c515cf308f38515039f4ad
Alexander <iam.asm89@gmail.com>
2011-12-16 17:13:25 +0100

    Comments via REST controller

--- Controller/Rest/ThreadController.php
+++ Controller/Rest/ThreadController.php
@@ -38,26 +38,6 @@ class ThreadController extends Controller
     }
 
     /**
-     * Get the comments of a thread.
-     *
-     * @todo Add support page/pagesize/sorting/tree-depth parameters
-     *
-     * @param string $id
-     * @return View
-     */
-    public function getThreadCommentsAction($id)
-    {
-        $thread = $this->container->get('fos_comment.manager.thread')->findThreadById($id);
-        $comments = $this->container->get('fos_comment.manager.comment')->findCommentTreeByThread($thread);
-
-        $view = View::create()
-          ->setStatusCode(200)
-          ->setData(array('comments' => $comments));
-
-        return $this->get('fos_rest.view_handler')->handle($view);
-    }
-
-    /**
      * Presents the form to use to create a new Thread.
      *
      * @return View
@@ -91,17 +71,17 @@ class ThreadController extends Controller
 
             if ($form->isValid()) {
                 if (null !== $this->container->get('fos_comment.manager.thread')->findThreadById($thread->getId())) {
-                    $this->onCreateErrorDuplicate($form);
+                    $this->onCreateThreadErrorDuplicate($form);
                 }
 
                 // Add the thread
                 $this->container->get('fos_comment.manager.thread')->addThread($thread);
 
-                $this->onCreateSuccess($form);
+                $this->onCreateThreadSuccess($form);
             }
         }
 
-        $this->onCreateError($form);
+        $this->onCreateThreadError($form);
     }
 
     /**
@@ -110,7 +90,7 @@ class ThreadController extends Controller
      * @param CommentForm $form
      * @return Response
      */
-    protected function onCreateSuccess(Form $form)
+    protected function onCreateThreadSuccess(Form $form)
     {
         return $this->container->get('http_kernel')->forward('FOSCommentBundle:Rest/Thread:getThread', array(
             'id' => $form->getData()->getId(),
@@ -124,7 +104,7 @@ class ThreadController extends Controller
      * @param CommentForm $form
      * @return Response
      */
-    protected function onCreateError(Form $form)
+    protected function onCreateThreadError(Form $form)
     {
         return new Response('An error occurred with form submission', 400);
     }
@@ -135,8 +115,114 @@ class ThreadController extends Controller
      * @param CommentForm $form
      * @return Response
      */
-    protected function onCreateErrorDuplicate(Form $form)
+    protected function onCreateThreadErrorDuplicate(Form $form)
     {
         return new Response(sprintf("Duplicate thread id '%s'.", $form->getData()->getId()), 400);
+    }
+
+    /**
+     * Get the comments of a thread.
+     *
+     * @todo Add support page/pagesize/sorting/tree-depth parameters
+     *
+     * @param string $id
+     * @return View
+     */
+    public function getThreadCommentsAction($id)
+    {
+        $thread = $this->container->get('fos_comment.manager.thread')->findThreadById($id);
+        $comments = $this->container->get('fos_comment.manager.comment')->findCommentTreeByThread($thread);
+
+        $view = View::create()
+          ->setStatusCode(200)
+          ->setData(array('comments' => $comments));
+
+        return $this->get('fos_rest.view_handler')->handle($view);
+    }
+
+
+    /**
+     * Presents the form to use to create a new Comment for a Thread.
+     *
+     * @param string id
+     * @return View
+     */
+    public function newThreadCommentsAction($id)
+    {
+        $thread = $this->container->get('fos_comment.manager.thread')->findThreadById($id);
+        if (!$thread) {
+            throw new NotFoundHttpException(sprintf('Thread with identifier of "%s" does not exist', $id));
+        }
+
+        $comment = $this->container->get('fos_comment.manager.comment')->createComment($thread);
+
+        $form = $this->container->get('fos_comment.form_factory.comment')->createForm();
+        $form->setData($comment);
+
+        $view = View::create()
+          ->setStatusCode(200)
+          ->setData(array(
+              'form' => $form->createView(),
+              'first' => 0 === $thread->getNumComments(),
+              'thread' => $thread,
+              'fos_comment_create_action_path' => $this->get('router')->generate('fos_comment_post_thread_comments', array('id' => $id)),
+              )
+          )
+          ->setTemplate(new TemplateReference('FOSCommentBundle', 'Comment', 'new'));
+
+        return $view;
+    }
+
+    /**
+     * Creates a new Comment for the Thread from the submitted data.
+     *
+     * @todo Add support for comment parent (in form)
+     *
+     * @param string $id
+     * @return Response
+     */
+    public function postThreadCommentsAction($id)
+    {
+        $thread = $this->container->get('fos_comment.manager.thread')->findThreadById($id);
+        if (!$thread) {
+            throw new NotFoundHttpException(sprintf('Thread with identifier of "%s" does not exist', $id));
+        }
+
+        $comment = $this->container->get('fos_comment.manager.comment')->createComment($thread);
+
+        $form = $this->container->get('fos_comment.form_factory.comment')->createForm();
+        $form->setData($comment);
+        $form->bindRequest($this->container->get('request'));
+
+        if ($form->isValid() && $this->container->get('fos_comment.creator.comment')->create($comment)) {
+            return $this->onCreateCommentSuccess($form);
+        }
+
+        return $this->onCreateCommentError($form);
+    }
+
+    /**
+     * Forwards the action to the thread view on a successful form submission.
+     *
+     * @param CommentForm $form
+     * @return Response
+     */
+    protected function onCreateCommentSuccess(Form $form)
+    {
+        return $this->container->get('http_kernel')->forward('FOSCommentBundle:Rest/Thread:getThreadComments', array(
+            'id' => $form->getData()->getThread()->getId(),
+            '_format' => 'json' // todo: change this when other formats are implemented
+        ));
+    }
+
+    /**
+     * Returns a 400 response when the form submission fails.
+     *
+     * @param CommentForm $form
+     * @return Response
+     */
+    protected function onCreateCommentError(Form $form)
+    {
+        return new Response('An error occurred with form submission', 400);
     }
 }
