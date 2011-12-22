29ccae3bfe51b40750ce21c4f13bc3612b50fb39
Alexander <iam.asm89@gmail.com>
2011-12-22 17:14:45 +0100

    Updated ThreadInterface/Acl

--- Model/ThreadManagerInterface.php
+++ Model/ThreadManagerInterface.php
@@ -11,6 +11,8 @@
 
 namespace FOS\CommentBundle\Model;
 
+use Symfony\Component\HttpFoundation\ParameterBag;
+
 /**
  * Interface to be implemented by comment thread managers. This adds an additional level
  * of abstraction between your application, and the actual repository.
@@ -49,6 +51,16 @@ interface ThreadManagerInterface
      * @return Thread
      */
     function createThread($id = null);
+
+    /**
+     * Creates a thread from the request parameters.
+     *
+     * @param string $id The id for the thread.
+     * @param ParameterBag $query The query parameters.
+     *
+     * @return ThreadInterface
+     */
+    function createThreadFromQuery($id, ParameterBag $query);
 
     /**
      * Saves a thread
