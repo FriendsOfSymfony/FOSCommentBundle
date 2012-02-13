d9f259a5b80c7e93ce023aed4b2697ca913abde2
Christophe Coevoet <stof@notk.org>
2012-02-13 10:29:17 +0100

    Changed vendors to git:// for Travis as http:// fails

--- vendor/vendors.php
+++ vendor/vendors.php
@@ -18,13 +18,13 @@ if (isset($argv[1])) {
 
 $vendorDir = __DIR__;
 $deps = array(
-    array('symfony', 'http://github.com/symfony/symfony', isset($_SERVER['SYMFONY_VERSION']) ? $_SERVER['SYMFONY_VERSION'] : 'origin/master'),
-    array('twig', 'http://github.com/fabpot/Twig.git', 'origin/master'),
-    array('doctrine-common', 'http://github.com/doctrine/common.git', 'origin/master'),
-    array('doctrine-dbal', 'http://github.com/doctrine/dbal.git', 'origin/master'),
-    array('doctrine', 'http://github.com/doctrine/doctrine2.git', 'origin/master'),
-    array('doctrine-mongodb-odm', 'http://github.com/doctrine/mongodb-odm.git', 'origin/master'),
-    array('doctrine-mongodb', 'http://github.com/doctrine/mongodb.git', 'origin/master'),
+    array('symfony', 'git://github.com/symfony/symfony.git', isset($_SERVER['SYMFONY_VERSION']) ? $_SERVER['SYMFONY_VERSION'] : 'origin/master'),
+    array('twig', 'git://github.com/fabpot/Twig.git', 'origin/master'),
+    array('doctrine-common', 'git://github.com/doctrine/common.git', 'origin/master'),
+    array('doctrine-dbal', 'git://github.com/doctrine/dbal.git', 'origin/master'),
+    array('doctrine', 'git://github.com/doctrine/doctrine2.git', 'origin/master'),
+    array('doctrine-mongodb-odm', 'git://github.com/doctrine/mongodb-odm.git', 'origin/master'),
+    array('doctrine-mongodb', 'git://github.com/doctrine/mongodb.git', 'origin/master'),
 );
 
 foreach ($deps as $dep) {
