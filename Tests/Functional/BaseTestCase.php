8a7cc987de47e118d659671c7504a9cbb025142e
Tim Nagel <tim@nagel.com.au>
2012-02-26 18:52:40 +1100

    Functional testing

---
+++ Tests/Functional/BaseTestCase.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace FOS\CommentBundle\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class BaseTestCase extends WebTestCase
+{
+    static protected function createKernel(array $options = array())
+    {
+        return new AppKernel(
+            isset($options['config']) ? $options['config'] : 'default.yml'
+        );
+    }
+}
---
+++ Tests/Functional/Bundle/CommentBundle/CommentBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace FOS\CommentBundle\Tests\Functional\Bundle\CommentBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CommentBundle extends Bundle
+{
+
+}
