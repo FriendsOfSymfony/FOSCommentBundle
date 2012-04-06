1cc5b106fb0e3371aae7e2747b36a4f7d2702ea8
Tim Nagel <tim@nagel.com.au>
2012-04-06 20:32:11 +1000

    Moved set up to before_script

--- run_tests.sh
+++ run_tests.sh
@@ -4,10 +4,4 @@ set -e
 set -o pipefail
 
 phpunit
-
-Tests/Functional/app/console assets:install --symlink Tests/Functional/web
-Tests/Functional/app/console doctrine:database:drop --force
-Tests/Functional/app/console doctrine:database:create
-Tests/Functional/app/console doctrine:schema:create
-
 Tests/Functional/app/console behat
