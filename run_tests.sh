c61c479428b912e29bfc8f6287ab41398c283afb
Tim Nagel <tim@nagel.com.au>
2012-11-24 15:30:23 +0100

    Updated test scripts a bit

--- run_tests.sh
+++ run_tests.sh
@@ -4,4 +4,11 @@ set -e
 set -o pipefail
 
 phpunit --coverage-text --colors
+
+echo ""
+echo "==============================="
+echo "BEHAT:"
+echo ""
+
+ 
 Tests/Functional/app/console behat --format=progress
