9f77508bcd76441e6263538eec90287fea9410e2
Karel Souffriau <karel@hardware.info>
2013-01-09 22:58:05 +0100

    CommentExtensionTest::testCanDeleteWhenNoCommentAcl fix

--- Tests/Twig/CommentExtensionTest.php
+++ Tests/Twig/CommentExtensionTest.php
@@ -157,7 +157,7 @@ class CommentExtensionTest extends \PHPUnit_Framework_TestCase
     {
         $comment = $this->getMock('FOS\CommentBundle\Model\CommentInterface');
         $extension = new CommentExtension();
-        $this->assertTrue($extension->canDeleteComment($comment));
+        $this->assertFalse($extension->canDeleteComment($comment));
     }
 
     public function testCanDeleteWhenCommentAclCanDelete()
