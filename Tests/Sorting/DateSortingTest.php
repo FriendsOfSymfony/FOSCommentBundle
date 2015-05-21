4b8a8cd42d30a8d3a748d31c09c579c356f1bc36
Karel Souffriau <karel@hardware.info>
2015-05-21 13:23:49 +0200

    Add date sorting test

---
+++ Tests/Sorting/DateSortingTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace FOS\CommentBundle\Tests\Sorting;
+
+use FOS\CommentBundle\Sorting\DateSorting;
+
+class DateSortingTest extends \PHPUnit_Framework_TestCase
+{
+    private $sorterAsc;
+    private $sorterDesc;
+
+    public function setUp()
+    {
+        $this->sorterAsc = new DateSorting('ASC');
+        $this->sorterDesc = new DateSorting('DESC');
+    }
+
+    public function testEqual()
+    {
+        $comment1 = $this->getMock('FOS\CommentBundle\Model\CommentInterface');
+        $comment2 = $this->getMock('FOS\CommentBundle\Model\CommentInterface');
+
+        $comment1->expects($this->any())
+            ->method('getCreatedAt')
+            ->will($this->returnValue(new \DateTime('2015-09-20 12:39:10')));
+
+        $comment2->expects($this->any())
+            ->method('getCreatedAt')
+            ->will($this->returnValue(new \DateTime('2015-09-20 12:39:10')));
+
+        $this->assertEquals(0, $this->sorterAsc->doSort(array('comment' => $comment1), array('comment' => $comment2)));
+        $this->assertEquals(0, $this->sorterDesc->doSort(array('comment' => $comment1), array('comment' => $comment2)));
+    }
+
+    public function testGreaterOrLess()
+    {
+        $comment1 = $this->getMock('FOS\CommentBundle\Model\CommentInterface');
+        $comment2 = $this->getMock('FOS\CommentBundle\Model\CommentInterface');
+
+        $comment1->expects($this->any())
+            ->method('getCreatedAt')
+            ->will($this->returnValue(new \DateTime('2015-08-20 12:39:10')));
+
+        $comment2->expects($this->any())
+            ->method('getCreatedAt')
+            ->will($this->returnValue(new \DateTime('2015-09-20 12:39:10')));
+
+        $this->assertEquals(-1, $this->sorterAsc->doSort(array('comment' => $comment1), array('comment' => $comment2)));
+        $this->assertEquals(1, $this->sorterAsc->doSort(array('comment' => $comment2), array('comment' => $comment1)));
+        $this->assertEquals(-1, $this->sorterDesc->doSort(array('comment' => $comment2), array('comment' => $comment1)));
+        $this->assertEquals(1, $this->sorterDesc->doSort(array('comment' => $comment1), array('comment' => $comment2)));
+    }
+}
