9adea090ce795d47c8675d4dc4e97c4fc18400fc
Karel Souffriau <karel@hardware.info>
2017-06-07 15:55:11 +0200

    Apply CS fixer

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,9 +1,18 @@
 <?php
 
+/*
+ * This file is part of the FOSCommentBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 $file = __DIR__.'/../vendor/autoload.php';
 if (!file_exists($file)) {
     throw new RuntimeException('Install dependencies using composer to run the test suite.');
 }
 
 $autoload = require $file;
-Doctrine\Common\Annotations\AnnotationRegistry::registerLoader([$autoload, 'loadClass']);
+Doctrine\Common\Annotations\AnnotationRegistry::registerLoader(array($autoload, 'loadClass'));
