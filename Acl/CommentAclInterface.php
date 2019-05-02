966deca3139d76d4e50c6b97a21ed45099cbcbbc
XWB <karelke@gmail.com>
2019-05-02 18:16:42 +0200

    Fixed phpdoc notice

--- Acl/CommentAclInterface.php
+++ Acl/CommentAclInterface.php
@@ -41,7 +41,7 @@ interface CommentAclInterface
      * Checks if the user can reply to the supplied 'parent' comment
      * or if not supplied, just the ability to reply.
      *
-     * @param CommentInterface $parent
+     * @param CommentInterface|null $parent
      *
      * @return bool
      */
